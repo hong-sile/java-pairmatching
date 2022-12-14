06803390ad3622dd229616b7be456d3ea170f532
hong-sile <gurwns9325@naver.com>
2022-12-14 23:31:26 +0900

    feat: 파일을 읽어 크루들의 이름을 List로 반환하는 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,10 +1,18 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import pairmatching.constant.CommandCode;
 
 public class InputView {
     private static final String SELECT_COMMAND_LIST_MESSAGE = "기능을 선택하세요.";
+    private static final String RESOURCES_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\";
+    private static final String BACKEND_CREW_FILE_NAME = "backend-crew.md";
+    private static final String FRONTEND_CREW_FILE_NAME = "frontend-crew.md";
 
     public static CommandCode readCommandCode() {
         System.out.println(SELECT_COMMAND_LIST_MESSAGE);
@@ -13,5 +21,27 @@ public class InputView {
         }
         String input = Console.readLine();
         return CommandCode.from(input);
+    }
+
+    public static List<String> readBackCrew() throws IOException {
+        List<String> crewNames = new ArrayList<>();
+        readFile(crewNames, BACKEND_CREW_FILE_NAME);
+        return crewNames;
+    }
+
+    public static List<String> readFrontCrew() throws IOException {
+        List<String> crewNames = new ArrayList<>();
+        readFile(crewNames, FRONTEND_CREW_FILE_NAME);
+        return crewNames;
+    }
+
+    private static void readFile(List<String> crewNames, String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new FileReader(RESOURCES_PATH + filePath)
+        );
+        String userName;
+        while ((userName = reader.readLine()) != null) {
+            crewNames.add(userName);
+        }
     }
 }
